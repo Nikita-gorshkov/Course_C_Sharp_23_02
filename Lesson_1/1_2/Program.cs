3f49ac316dafcc47a06166a73b608b4bc5e92859
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-01 01:32:19 +0300

    Task_1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Hello");
+
+int a = int.Parse(Console.ReadLine()!);
+
+if (a == 1)
+{
+    Console.WriteLine("Mondey");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Th");
+}
+else if (a == 3)
+{
+    Console.WriteLine("sreda");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Chetverg");
+}
+else if (a == 5)
+{
+    Console.WriteLine("Pyatnica");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Sub");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Voskresenie");
+}
+else
+{
+    Console.WriteLine("FALSE");
+}
