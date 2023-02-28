7c20d38a642171826d9669851ca2bb242f380447
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-01 01:55:45 +0300

    Task_1_3_end

--- Lesson_1/1_3/Program.cs
+++ Lesson_1/1_3/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = int.Parse(Console.ReadLine()!);
+int b = -a;
+while (b <= a)
+{
+   Console.Write($"{b} ");
+   b++; 
+}
