aa19ccaf0c11640362f53967aaf6abbc1ed0d821
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-01 00:04:50 +0300

    Task_1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.Readline()!);
+int b = int.Parse(Console.Readline()!);
+if (b == a * a)
+{
+ Console.WriteLine("Yes");  
+
+}
+else
+{
+    Console.WriteLine("No");  
+    
+}
