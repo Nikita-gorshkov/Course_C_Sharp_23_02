8eb2326efb3cbbe4dc267b85012651725df11529
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-02 17:22:32 +0300

    HW zadacha 1

---
+++ Lesson_1/HW/HW_1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vvedite 2 chisla:");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.Write("Max=");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Max=");
+    Console.WriteLine(b);
+}
