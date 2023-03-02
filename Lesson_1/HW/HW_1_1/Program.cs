e027277f62f63c915ffbb3ce791195bb94bf764c
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-02 17:40:22 +0300

    HW zadacha 2

--- Lesson_1/HW/HW_1_1/Program.cs
+++ Lesson_1/HW/HW_1_1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Vvedite 2 chisla:");
 int a = int.Parse(Console.ReadLine()!);
 int b = int.Parse(Console.ReadLine()!);
---
+++ Lesson_1/HW/HW_1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих
+Console.WriteLine("Vvedi 3 chisla:");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int d = int.Parse(Console.ReadLine()!);
+
+int Max = a;
+if (a > Max)
+{
+    Max = a;
+}
+if (b > Max)
+{
+    Max = b;
+}
+if (d > Max)
+{
+    Max = d;
+}
+Console.Write("Max=");
+    Console.WriteLine(Max);
