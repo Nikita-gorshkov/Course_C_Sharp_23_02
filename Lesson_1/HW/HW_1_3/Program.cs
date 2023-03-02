f888ed9c96688447613b96c4a256168597b7151b
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-02 18:03:38 +0300

    HW zadacha 3

---
+++ Lesson_1/HW/HW_1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Vvedi luboe chislo:");
+int a = int.Parse(Console.ReadLine()!);
+if (a%2 == 0)
+{
+   Console.WriteLine("CHETNOE"); 
+}
+else
+{
+   Console.WriteLine("NE chetnoe"); 
+}
