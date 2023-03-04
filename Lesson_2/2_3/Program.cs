03f1d0384258952f8fb5ad0868758cb0a3602abe
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-04 22:34:19 +0300

    Task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2 числа. Если одно кратно второму то КРАТНО, если не кратно, НЕ кратно и остаток от деления пишем
+Console.WriteLine("Zadacha 3");
+
+void Div(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+         Console.WriteLine("Kratno");
+    else
+        Console.WriteLine("NE Kratno " + num1 % num2);
+}
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Div(a, b);
