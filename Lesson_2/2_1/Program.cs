08ce06105a6589ca619939997636deb0a9b9ae5e
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-04 19:36:14 +0300

    Task 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Прога показывает последнюю цифру трёхзначного числа
+Console.WriteLine("Zadacha 1");
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int rezult = TakeNum(new Random().Next(100, 1000));
+
+Console.WriteLine(rezult);
+
