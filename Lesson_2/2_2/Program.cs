4fa0003a014d4b0cd816c9ca4395cfa0c1ff546e
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-04 21:11:25 +0300

    Task 2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести случ число от 10 до 99 и в нём показать большую цифру
+Console.WriteLine("Zadacha 2");
+
+int MaxNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10; 
+    int num2 = num % 10; 
+    if (num1 > num2)
+    return num1;
+    
+    return num2;
+}
+
+int rezult = MaxNum(new Random().Next(10, 100));
+
+Console.WriteLine(rezult);
