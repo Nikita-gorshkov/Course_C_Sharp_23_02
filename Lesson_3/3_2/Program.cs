376b51064a6a9efc01b22dd7db3480add78184f3
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-06 01:13:51 +0300

    Task_3_2

--- Lesson_3/3_2/Program.cs
+++ Lesson_3/3_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// SПринимает координаты 2-х точек, а выдаёт расстояние между ними.
+
+
+Console.WriteLine("Vvedite koordinati 2 tochek:");
+
+double dis(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 -x1,2) + Math.Pow(y2 -y1,2));
+}
+Console.WriteLine(dis(3, 6, 2, 1));
