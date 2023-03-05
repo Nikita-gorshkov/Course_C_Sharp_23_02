63322ae3219f5269331cc4b1783ebdc1ea873bf1
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-06 00:28:18 +0300

    Task_3_1

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Koordinatnaya ploskost");
+void Quarters(int num)
+{
+    if (num == 1)
+        Console.WriteLine("x>0, y>0");
+    else if (num == 2)
+        Console.WriteLine("x<0, y>0");
+    else if (num == 3)
+        Console.WriteLine("x<0, y<0");
+    else if (num == 4)
+        Console.WriteLine("x>0, y<0");
+    else 
+        Console.WriteLine("Error");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+
+Quarters(num);
