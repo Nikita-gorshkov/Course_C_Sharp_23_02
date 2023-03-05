3a4ee5f85128364f45511f5bdf6f7e8515a3b862
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-05 18:34:47 +0300

    Task HW_2_3

---
+++ Lesson_2/HW/HW_2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Zadacha_3");
+
+void TakeNum (int num)
+{
+    if (num <= 99)
+    {
+        Console.WriteLine("NET 3 cifri");
+    }
+    else
+    {
+        while (num > 999)
+        num /= 10;
+        Console.WriteLine(num % 10);
+    }
+}
+TakeNum(int.Parse(Console.ReadLine()!));
+
+
+
