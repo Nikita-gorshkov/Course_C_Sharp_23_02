f3526321cb1de142ea673ae0b0cc5022e06434cd
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-05 17:34:45 +0300

    Task HW_2_2

---
+++ Lesson_2/HW/HW_2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//   Напишите программу, которая выводит
+//   случайное трёхзначное число и удаляет вторую цифру
+//   этого числа.
+
+Console.WriteLine("Zadacha_2");
+
+void GivNum (int num)
+{
+    Console.WriteLine(num);
+    Console.WriteLine($"{num / 100}");
+    Console.WriteLine($"{num % 10}");
+}
+  int num = new Random().Next(100, 1000);
+
+GivNum(num);
