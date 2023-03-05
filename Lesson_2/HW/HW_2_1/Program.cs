1e1847666c817dfe9aad8b30e37e8e735bc786b7
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-05 15:51:38 +0300

    Task HW_2_1

---
+++ Lesson_2/HW/HW_2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Zadacha 2_1   VVEDI 3-h znachnoe chislo:");
+
+int TakeNum(int num)
+{
+   
+    return num /10 % 10;
+}
+
+int rezult = TakeNum(int.Parse(Console.ReadLine()!));
+
+Console.WriteLine(rezult);
