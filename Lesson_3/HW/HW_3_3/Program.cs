153f8ca8d960ed2be70261a8fee25bd29f9f9ae1
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-06 19:37:19 +0300

    HW_3_3

--- Lesson_3/HW/HW_3_3/Program.cs
+++ Lesson_3/HW/HW_3_3/Program.cs
@@ -1,7 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.WriteLine("Vvedite chislo: ");
 
-int TakeNum(int num)
+void TakeNum(int num)
 {
-    
-}
+    double[] number = new double[num];
+    int i = 0;
+
+    while (i < num)
+    {
+        number[i] = Math.Pow((i + 1), 3);
+        Console.Write($"{number[i]}, ");
+        i=i+1;
+    }
+} 
+Console.WriteLine();
+
+TakeNum(int.Parse(Console.ReadLine()!));
