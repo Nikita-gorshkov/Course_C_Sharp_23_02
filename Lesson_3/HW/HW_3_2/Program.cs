ddc26a49b0dba59c6e5af5a56b98b18bbbf921a8
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-06 18:36:30 +0300

    HW_3_2+ROUND

--- Lesson_3/HW/HW_3_2/Program.cs
+++ Lesson_3/HW/HW_3_2/Program.cs
@@ -10,7 +10,7 @@ double z2 = double.Parse(Console.ReadLine()!);
 
 double dis3D(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-    return Math.Sqrt(Math.Pow(x2 -x1, 2) + Math.Pow(y2 -y1, 2) + Math.Pow(z2 -z1, 2));
+    return Math.Round(Math.Sqrt(Math.Pow(x2 -x1, 2) + Math.Pow(y2 -y1, 2) + Math.Pow(z2 -z1, 2)), 2);
 }
 
 Console.WriteLine(dis3D(x1, y1, z1, x2, y2, z2));
