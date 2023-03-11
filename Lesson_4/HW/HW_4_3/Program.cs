048be4d1655c3861d9f64bb1085519479db117d0
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-11 20:10:08 +0300

    HW_4_3

---
+++ Lesson_4/HW/HW_4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+void Massif(int[] mas)
+   {
+    for (int i = 0; i < mas.Length; i++ )
+        Console.Write($"{mas[i]} ");
+   }
+
+int[] MasVvod(int start, int finish, int size)
+   {
+    int[]mas = new int[size];
+    for (int i = 0; i < size; i++)
+        mas[i] = new Random().Next(start, finish);
+    return mas;
+   }
+
+Console.Write("Vvedi OT: ");
+   int ot = int.Parse(Console.ReadLine()!);
+Console.Write("Vvedi TO: ");
+   int to = int.Parse(Console.ReadLine()!);
+Console.Write("Vvedi DLINA: ");   
+   int dlina = int.Parse(Console.ReadLine()!);
+
+   int[] massss = MasVvod(ot, to, dlina);
+   Massif(massss);
