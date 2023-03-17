df5d312c487e158817f808d52d703cfa9816c9f0
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-18 01:03:04 +0300

    HW_6_1

---
+++ Lesson_6/HW/HW_6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Приветик! Сколько хочешь вввести:");
+
+int Nombers(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write("Ну вводи свои числа:");
+        int a = int.Parse(Console.ReadLine()!);
+        if (a > 0)
+        count = count + 1;
+    }
+      return count;
+}
+
+int chislo = int.Parse(Console.ReadLine()!);
+int result = Nombers(chislo);
+Console.Write($"А я знаю, что, больше нудя чисел аж ");
+Console.WriteLine(result);
