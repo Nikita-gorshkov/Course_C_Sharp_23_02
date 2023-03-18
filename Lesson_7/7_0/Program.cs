688f96708138d441ec5ec647bd14401b0c4ac0cb
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-19 00:34:30 +0300

    Task_7_0

---
+++ Lesson_7/7_0/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+
+void Print(int[,] arr)
+   {
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++ )
+    {
+        for (int j = 0; j < column; j++ )
+             Console.Write($"{arr[i,j]} ");
+        Console.WriteLine();
+    }
+   }
+
+int[,] MasNums(int row, int column, int from, int to)
+   {
+    int[,]arr = new int[row, column];
+
+    for (int i = 0; i < row; i++ )
+    {
+        for (int j = 0; j < column; j++ ) 
+        arr[i,j] = new Random().Next(from, to);
+    }
+    return arr;
+   }
+
+Console.Write("Vvedi row_num: ");
+   int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Vvedi column_num: ");
+   int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Vvedi start: ");
+   int start = int.Parse(Console.ReadLine()!);
+Console.Write("Vvedi stop: ");   
+   int stop = int.Parse(Console.ReadLine()!);
+
+   int[,] mass = MasNums(row_num, column_num, start, stop);
+   Print(mass); 
+    
