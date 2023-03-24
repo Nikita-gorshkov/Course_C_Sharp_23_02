898e68ff399edb45d9e57e3dff2f516f39e2dc18
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-24 18:01:43 +0300

    HW_9_2_end

--- Lesson_9/HW/HW_9_2/Program.cs
+++ Lesson_9/HW/HW_9_2/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
 // натуральных элементов в промежутке от M до N с помощью рекурсии.
-void NumsFromTo(int M, int N)
+int Sum(int M, int N)
 {
-    if (M > N) return;
-    NumsFromTo(M, N - 1);
-    System.Console.Write($"{N}, ");
+    if (M > N) return 0;
+    return Sum(M, N - 1) + N;
+      
 }
-System.Console.Write("Введите первое число: ");
+Console.Write("Введите первое число: ");
 int Num = int.Parse(Console.ReadLine()!);
-System.Console.Write("Введите второе число: ");
+Console.Write("Введите второе число: ");
 int Num2 = int.Parse(Console.ReadLine()!);
 
-
-NumsFromTo(Num, Num2);
+Console.Write("Сумма натуральных элементов: ");
+Console.WriteLine(Sum(Num, Num2));
