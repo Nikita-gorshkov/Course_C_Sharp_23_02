74e0ddcac7bd7bc67aef77ba208e6e2adacf4b89
Nikita Gorshkov <nikita-gorshkov@yandex.ru>
2023-03-24 17:46:35 +0300

    HW_9_1_end

--- Lesson_9/HW/HW_9_1/Program.cs
+++ Lesson_9/HW/HW_9_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
 
+void NumsFromTo(int M, int N)
+{
+    if (M > N) return;
+
+        if(N%2 == 0) 
+            {
+               NumsFromTo(M, N - 2);
+               Console.Write($"{N} "); 
+            } 
+            
+           else if (N%2 != 0)
+            {
+             N = N - 1;
+             NumsFromTo(M, N - 2);
+
+             Console.Write($"{N} "); 
+            }
+                                  
+}
+
+
+Console.Write("Введите первое число: ");
+int Num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int Num2 = int.Parse(Console.ReadLine()!);
+
+
+NumsFromTo(Num, Num2);
